c041e0ffdb81816b8afe7fcf40dd7f7e8147a087
Soumyadeep Biswas <soumyadeep184@gmail.com>
2022-05-17 21:14:47 +0530

    Create queue_using_array.cpp

---
+++ Queue/queue_using_array.cpp
@@ -0,0 +1,91 @@
+// @author Soumya
+#include <iostream>
+using namespace std;
+
+#define n 20
+
+class queue
+{
+    int *arr;
+    int front, back;
+
+public:
+    queue()
+    {
+        arr = new int[n];
+        front = -1;
+        back = -1;
+    }
+
+    void enqueue(int x)
+    {
+        if (back == n - 1)
+        {
+            cout << "Queue overflow!!" << endl;
+            return;
+        }
+
+        back++;
+        arr[back] = x;
+
+        if (front == -1)
+            front++;
+    }
+
+    void dequeue()
+    {
+        if (front == -1 || front > back)
+        {
+            cout << "No element in queue!!" << endl;
+            return;
+        }
+        front++;
+    }
+
+    int peek()
+    {
+        if (front == -1 || front > back)
+        {
+            cout << "NO element in queue!!" << endl;
+            return -1;
+        }
+        return arr[front];
+    }
+
+    bool empty()
+    {
+        if (front == -1 || front > back)
+            return true;
+        return false;
+    }
+};
+
+int main()
+{
+    queue q;
+
+    q.enqueue(1);
+    q.enqueue(2);
+    q.enqueue(3);
+    q.enqueue(4);
+    q.enqueue(5);
+
+    cout << q.peek() << endl;
+    q.dequeue();
+
+    cout << q.peek() << endl;
+    q.dequeue();
+
+    cout << q.peek() << endl;
+    q.dequeue();
+
+    cout << q.peek() << endl;
+    q.dequeue();
+
+    cout << q.peek() << endl;
+    q.dequeue();
+
+    cout << q.empty() << endl;
+
+    return 0;
+}
