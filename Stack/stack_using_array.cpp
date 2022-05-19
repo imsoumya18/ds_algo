6f9465884f5a4f7711f03fb38fc06c369b713c4b
Soumyadeep Biswas <soumyadeep184@gmail.com>
2022-05-19 06:36:56 +0530

    Create stack_using_array.cpp

---
+++ Stack/stack_using_array.cpp
@@ -0,0 +1,81 @@
+// @author Soumya
+#include <iostream>
+using namespace std;
+
+#define n 100
+
+class stack
+{
+    int *arr;
+    int top;
+
+public:
+    stack()
+    {
+        arr = new int[n];
+        top = -1;
+    }
+
+    void push(int x)
+    {
+        if (top == n - 1)
+        {
+            cout << "Stack overflow!!" << endl;
+            return;
+        }
+
+        top++;
+        arr[top] = x;
+    }
+
+    void pop()
+    {
+        if (top == -1)
+        {
+            cout << "Stack underflow!!" << endl;
+            return;
+        }
+
+        top--;
+    }
+
+    int peek()
+    {
+        if (top == -1)
+        {
+            cout << "Stack is empty!!" << endl;
+            return -1;
+        }
+
+        return arr[top];
+    }
+
+    bool empty()
+    {
+        return top == -1;
+    }
+};
+
+int main()
+{
+    stack st;
+
+    st.push(1);
+    st.push(2);
+    st.push(3);
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    cout << st.peek() << endl;
+    st.pop();
+
+    st.pop();
+
+    cout << st.empty() << endl;
+
+    return 0;
+}
