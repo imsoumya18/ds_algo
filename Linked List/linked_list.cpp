daf2fa50785b216eaeb485d9fef920338ddd6816
Soumyadeep Biswas <soumyadeep184@gmail.com>
2022-05-31 19:14:20 +0530

    Create linked_list.cpp

---
+++ Linked List/linked_list.cpp
@@ -0,0 +1,63 @@
+// @author Soumya
+#include <iostream>
+using namespace std;
+
+// linked list implementation
+class node
+{
+public:
+    int data;
+    node *next;
+
+    node(int val)
+    {
+        data = val;
+        next = NULL;
+    }
+};
+
+// insert node at end
+void insertAtEnd(node *&head, int val)
+{
+    node *n = new node(val);
+
+    if (head == NULL)
+    {
+        head = n;
+        return;
+    }
+
+    node *temp = head;
+
+    while (temp->next != NULL)
+        temp = temp->next;
+
+    temp->next = n;
+}
+
+// print elements of linked list
+void display(node *head)
+{
+    node *temp = head;
+
+    while (temp != NULL)
+    {
+        cout << temp->data << "->";
+        temp = temp->next;
+    }
+    cout << "NULL" << endl;
+}
+
+int main()
+{
+    node *head = NULL;
+    insertAtEnd(head, 1);
+    insertAtEnd(head, 2);
+    insertAtEnd(head, 3);
+    insertAtEnd(head, 4);
+    insertAtEnd(head, 5);
+
+    display(head);
+
+    return 0;
+}
