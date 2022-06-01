ecf0440d7ef755dcf85b6ddc630188ed3eab512d
Soumyadeep Biswas <soumyadeep184@gmail.com>
2022-06-01 22:30:34 +0530

    Update bubble_sort.cpp

--- Sorting Algorithms/bubble_sort.cpp
+++ Sorting Algorithms/bubble_sort.cpp
@@ -8,7 +8,7 @@ using namespace std;
 /*Repeatedly swap two adjacent elements
 if they are in wrong order*/
 
-void selection_sort(vector<int> &vct)
+void bubble_sort(vector<int> &vct)
 {
     for (int i = 0; i < vct.size() - 1; i++)
         for (int j = 0; j < vct.size() - i - 1; j++)
@@ -20,7 +20,7 @@ int main()
 {
     vector<int> vct{12, 45, 23, 51, 19, 8};
 
-    selection_sort(vct);
+    bubble_sort(vct);
 
     for (auto i : vct)
         cout << i << " ";
