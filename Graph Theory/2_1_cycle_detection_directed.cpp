078231d8286bb912b66967b18a9e5a67e02f76f1
imsoumya18 <soumyadeep184@gmail.com>
2026-07-08 10:30:50 +0530

    cycle in directed graph

--- Graph Theory/2_1_cycle_detection_directed.cpp
+++ Graph Theory/2_1_cycle_detection_directed.cpp
@@ -38,6 +38,40 @@ bool isCycle_dfs(int V, vector<vector<int>> &adj)
     return false;
 }
 
+bool isCycle_bfs(int V, vector<vector<int>> &adj)
+{
+    vector<int> indegree(V, 0);
+
+    for (auto it : adj)
+        for (auto node : it)
+            indegree[node]++;
+
+    queue<int> q;
+
+    for (int i = 0; i < V; i++)
+        if (indegree[i] == 0)
+            q.push(i);
+
+    vector<int> vct;
+
+    while (!q.empty())
+    {
+        int node = q.front();
+        q.pop();
+        vct.push_back(node);
+
+        for (auto it : adj[node])
+        {
+            indegree[it]--;
+
+            if (indegree[it] == 0)
+                q.push(it);
+        }
+    }
+
+    return vct.size() != V;
+}
+
 int main()
 {
     int n, m, i;
