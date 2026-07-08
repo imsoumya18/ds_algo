0bdeb0fb6bb4ebd60ab03384ec96129fb0250b1f
imsoumya18 <soumyadeep184@gmail.com>
2026-07-08 10:10:08 +0530

    cycle in directed graph

---
+++ Graph Theory/2_1_cycle_detection_directed.cpp
@@ -0,0 +1,71 @@
+// @author Soumya
+#include <iostream>
+#include <vector>
+#include <stack>
+#include <queue>
+#include <algorithm>
+#include <climits>
+using namespace std;
+
+bool dfs(int node, vector<vector<int>> &adj, vector<int> &vis, vector<int> &dfs_vis)
+{
+    vis[node] = 1;
+    dfs_vis[node] = 1;
+
+    for (auto it : adj[node])
+        if (!vis[it])
+        {
+            if (dfs(it, adj, vis, dfs_vis))
+                return true;
+        }
+        else if (dfs_vis[it] == 1)
+            return true;
+
+    dfs_vis[node] = 0;
+
+    return false;
+}
+
+bool isCycle_dfs(int V, vector<vector<int>> &adj)
+{
+    vector<int> vis(V, 0), dfs_vis(V, 0);
+
+    for (int i = 0; i < V; i++)
+        if (!vis[i])
+            if (dfs(i, adj, vis, dfs_vis))
+                return true;
+
+    return false;
+}
+
+int main()
+{
+    int n, m, i;
+    cin >> n >> m;
+
+    vector<vector<int>> adj(n);
+
+    for (i = 0; i < m; i++)
+    {
+        int u, v;
+        cin >> u >> v;
+
+        adj[u].push_back(v);
+    }
+
+    for (i = 0; i < n; i++)
+    {
+        cout << i << "-->   ";
+        for (auto j : adj[i])
+            cout << j << " ";
+        cout << endl;
+    }
+    cout << endl;
+
+    if (isCycle_dfs(n, adj))
+        cout << "Cycle is there" << endl;
+    else
+        cout << "Cycle isn't there" << endl;
+
+    return 0;
+}
